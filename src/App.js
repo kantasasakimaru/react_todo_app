8984568a3de0ddf95369c34b6dd2465d97185d5d
kanta_sasaki <k_padbjw@icloud.com>
2020-04-06 01:03:25 +0900

    簡単なToDoアプリを作成してみた

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import pageOne from './component/pageOne';
-import pageTwo from './component/pageTwo';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: ""
+    };
+  }
+
+  // テキストボックスの内容が変化した場合にnewTodoという変数のStateが変更される
+  handleChange(e) {
+    this.setState({ newTodo: e.target.value });
+  }
+
+  // ADDボタンが押された時にもし内容がからだったらtodoをそのまま返し値があった場合はtodoにnewTodoを代入しtodosの値を更新する
+  // またnewTodoをからに戻す
+  addTodo() {
+    if (this.state.newTodo === "") return;
+    const todos = this.state.todos;
+    todos.push(this.state.newTodo);
+    this.setState({ todos: todos });
+    this.setState({ newTodo: "" });
+  }
+
+  // 引数番号の要素を削除し, todosのStateを変更する.
+  deletTodo(i) {
+    const todos = this.state.todos;
+    // spliceで消す配列番号を指定する. １をつけないとごそっとなくなるらいい
+    todos.splice(i, 1);
+    this.setState({ todos: todos });
+  }
 
   render() {
     return (
       <div>
-        <Route path="/" component={pageOne} exact={true} />
-        <Route path="/pagetwo" component={pageTwo} />
+        <h3>My ToDo</h3>
+        <input value={this.state.newTodo}
+          onChange={this.handleChange.bind(this)}
+          placeholder="Input here..." />
+        <button onClick={this.addTodo.bind(this)}>ADD</button>
+
+        <h5>ToDo List</h5>
+        <ul>
+          {
+            this.state.todos.map((todo, i) => {
+              return (
+                <li key={i}>
+                  {todo}
+                  <button onClick={this.deletTodo.bind(this, i)}>DEL</button>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
